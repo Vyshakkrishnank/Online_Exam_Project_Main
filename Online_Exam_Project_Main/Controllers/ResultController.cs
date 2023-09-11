703bc800eed41f14a352afc4a8b43afccd06789c
vyshak.krishnan <vyshak.krishnan@MS005>
2023-09-11 09:06:18 +0530

    result controller

--- Online_Exam_Project_Main/Controllers/ResultController.cs
+++ Online_Exam_Project_Main/Controllers/ResultController.cs
@@ -66,7 +66,7 @@ namespace Online_Exam_Project_Main.Controllers
             Session["quality"] = ViewBag.quality;
 
             ViewBag.candidate_id = 1;
-            ViewBag.exam_id = 1;
+            ViewBag.exam_id = Session["exid"];
             return View();
         }
 
